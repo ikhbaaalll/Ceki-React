231bb54eba11f9171ea45d464eeaea68df76fd90
ikhbaaalll <ikhbal.madrid17@gmail.com>
2023-02-02 02:55:34 +0700

    chore: create boilerplate

---
+++ conventional-commit.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+    commit: "commit",
+    host: "https://dev.azure.com/MSTApplications1",
+    repository: "Online%20Audit%20dan%20IH%20and%20E%20Measurement",
+    linkCompare: false,
+};
